f462c3fe4612ba730fd65415f98ba7adf8a52ed4
newtonkim <knewts98@gmail.com>
2022-11-29 14:41:23 +0300

    add toggle switching btw login and register

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -30,17 +30,31 @@
           <!-- Tabs -->
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition hover:text-white text-white
-                bg-blue-600" href="#">Login</a>
+                <a class="block rounded py-3 px-4 transition"  href="#" @click.prevent="tab = 'login'"
+                    :class="{
+                        'hover:text-white text-white bg-blue-600': tab === 'login',
+                        'hover:text-blue-600' : tab === 'register',
+                    }"
+                >
+                    Login
+                </a>
             </li>
             <li class="flex-auto text-center">
               <a class="block rounded py-3 px-4 transition" 
-                href="#">Register</a>
+                href="#"  @click.prevent="tab = 'register'"
+                :class="{
+                    'hover:text-white text-white bg-blue-600': tab === 'register',
+                    'hover:text-blue-600' : tab === 'login',
+                }"
+            >
+                Register
+            </a>
+              
             </li>
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -63,8 +77,9 @@
               Submit
             </button>
           </form>
+
           <!-- Registration Form -->
-          <form>
+          <form v-show="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -137,6 +152,11 @@ import { mapMutations, mapGetters} from 'vuex';
 
 export default {
     name: 'Auth',
+    data() {
+        return {
+            tab: 'login',
+        }
+    },
     computed: {
         ...mapGetters(['authModalShow']),
         // authModalShow() {
